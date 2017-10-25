eef77661a91977a0ca3ad11d0d94b0416c84d944
Jordan Mor <jordanmor@gmail.com>
2017-10-25 14:11:31 -0400

    Added currentDate function

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
+function addCurrentDate() {
+	const d = new Date();
+	const today = d.getDay();
+	const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+	return daysOfWeek[today];
+}
+
 function printMessage(name, city, carModel) {
-	console.log(`${name} lives in ${city} and drives a ${carModel}`);
+	console.log(`${name} lives in ${city} and drives a ${carModel}. Today is ${addCurrentDate()}.`);
 }
 
 printMessage('Colin', 'Hartford', 'Peugot');
