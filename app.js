7343c1f4fb6986931558db5c6e0e82a27e3b4e5e
Jordan Mor <jordanmor@gmail.com>
2017-10-26 17:35:48 -0400

    Added to company info function

--- app.js
+++ app.js
@@ -5,9 +5,57 @@ function addCurrentDate() {
 	return daysOfWeek[today];
 }
 
-function printMessage(name, city, carModel) {
-	return (`${name} lives in ${city} and drives a ${carModel}. Today is ${addCurrentDate()}.`);
+function Employees(name, city, carModel) {
+	this.name = name;
+	this.city = city;
+	this.carModel = carModel;
+	this.remote = true;
 }
 
-console.log(printMessage('Colin', 'Hartford', 'Peugot'));
+const employees = [
+	new Employees('Colin', 'Hartford', 'Peugot', true),
+ 	new Employees('Jordan', 'New Haven', 'Toyota', false),
+	new Employees('Emily', 'Stamford', 'Lexus', true)
+];
 
+function Company(employees, location) {
+	this.employees = employees;
+	this.location = location;
+}
+
+const telitest = new Company(employees, 'New Haven');
+
+// const employees = [
+// 	{
+// 		name: 'Colin',
+// 		city: 'Hartford',
+// 		carModel: 'Peugot'
+// 	},
+// 	{
+// 		name: 'Jordan',
+// 		city: 'New Haven',
+// 		carModel: 'Toyota'
+// 	},
+// 	{
+// 		name: 'Emily',
+// 		city: 'Stamford',
+// 		carModel: 'Lexus'
+// 	}
+// ];
+
+
+function printMessage(name, city, carModel) {
+	return (`${name} lives in ${city} and drives a ${carModel}. Today is ${addCurrentDate()}.\n`);
+}
+
+function printEmployeeInfo(company) {
+
+	for ( let i = 0,
+		  employee = company.employees,
+		  totalEmployees = company.employees.length; i < totalEmployees; i++) {
+
+		console.log(printMessage(employee[i].name, employee[i].city, employee[i].carModel));
+	}
+}
+
+printEmployeeInfo(telitest);
