4f746e026e1c4016e1ac284ff218aa403066f906
Jordan Mor <jordanmor@gmail.com>
2017-11-05 13:22:07 -0500

    Add app.js with bcrypt function in new hash dir

---
+++ hash/app.js
@@ -0,0 +1,11 @@
+let unsecurePlainTextPassword = 'password';
+var bcrypt = require('bcrypt');
+const saltRounds = 10;
+const myPlaintextPassword = 's0/\/\P4$$w0rD';
+const someOtherPlaintextPassword = 'not_bacon';
+
+bcrypt.genSalt(saltRounds, function(err, salt) {
+    bcrypt.hash(unsecurePlainTextPassword, salt, function(err, hash) {
+        console.log(hash);
+    });
+});
