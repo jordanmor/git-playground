aab681ef4ef6720571da43e28be04c165d04512c
Jordan Mor <jordanmor@gmail.com>
2017-11-05 14:14:36 -0500

    Initialize package.json file
    Install npm package colors as dependency
    Require colors in app.js

--- hash/app.js
+++ hash/app.js
@@ -1,11 +1,12 @@
 let unsecurePlainTextPassword = 'password';
 var bcrypt = require('bcrypt');
+var colors = require('colors');
 const saltRounds = 10;
 const myPlaintextPassword = 's0/\/\P4$$w0rD';
 const someOtherPlaintextPassword = 'not_bacon';
 
 bcrypt.genSalt(saltRounds, function(err, salt) {
     bcrypt.hash(unsecurePlainTextPassword, salt, function(err, hash) {
-        console.log(hash);
+        console.log(hash.green);
     });
 });
